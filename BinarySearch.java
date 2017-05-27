f5403e29eb69668beea014ad12329cd9d058c7d4
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-05-27 15:58:29 +0600

    Update and rename binarySearch.java to BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class binarySearch {
+public class BinarySearch {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
